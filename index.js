8df579d918ac6825b96f400675994ba0d1f6f604
CodeDotJS <rushforlinux@gmail.com>
2016-04-21 03:08:31 +0530

    init

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-#!/usr/bin/env node
-
 'use strict';
 
-const got = require('got');
+var cheerio = require('cheerio');
 
-const cheerio = require('cheerio');
+var Promise = require('pinkie-promise');
 
-const promise = require('pinkie-promise');
+var got = require('got');
 
-module.exports = function(username) {
+module.exports = function (username) {
 	if (typeof username !== 'string') {
 		return Promise.reject(new Error('username required'));
 	}
-};
 
+	var url = 'https://npmjs.com/~' + username;
+
+	return got(url).then(function (res) {
+		var $ = cheerio.load(res.body);
+
+		return {
+			name: $('.fullname').text() || null,
+
+			packages: $('#packages').text().replace(/ /g, '').replace('Packagesby', '').replace('\n', '').replace('\n', '').replace(username, '') || null
+		};
+	}).catch(function (err) {
+		if (err.statusCode === 404) {
+			err.message = 'Not a Twitter User.';
+		}
+
+		throw err;
+	});
+};
